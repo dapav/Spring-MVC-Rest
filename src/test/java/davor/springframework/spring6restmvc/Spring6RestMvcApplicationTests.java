ed10ad61b9957383131229657e500d3200df15b7
dapav <82372150+dapav@users.noreply.github.com>
2023-05-08 23:39:59 +0300

    First commit

---
+++ src/test/java/davor/springframework/spring6restmvc/Spring6RestMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package davor.springframework.spring6restmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6RestMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
