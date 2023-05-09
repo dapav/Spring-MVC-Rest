74c8db3edbc0263ec39c2d1c0269251ecf01f6cf
dapav <82372150+dapav@users.noreply.github.com>
2023-05-09 15:23:28 +0300

    Get a list of beers

--- src/main/java/davor/springframework/spring6restmvc/services/BeerService.java
+++ src/main/java/davor/springframework/spring6restmvc/services/BeerService.java
@@ -2,9 +2,12 @@ package davor.springframework.spring6restmvc.services;
 
 import davor.springframework.spring6restmvc.model.Beer;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerService {
+
+    List<Beer> listBeers();
 
     Beer getBeerById(UUID id);
 }
