5a25c32dfe47a3c4a513ce920083428354abdae6
dapav <82372150+dapav@users.noreply.github.com>
2023-05-10 19:35:33 +0300

    Mock test the getBeerByid

--- src/main/java/davor/springframework/spring6restmvc/Controller/BeerController.java
+++ src/main/java/davor/springframework/spring6restmvc/Controller/BeerController.java
@@ -3,6 +3,7 @@ package davor.springframework.spring6restmvc.Controller;
 import davor.springframework.spring6restmvc.model.Beer;
 import davor.springframework.spring6restmvc.services.BeerService;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -15,7 +16,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Slf4j
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/beer")
 public class BeerController {
--- src/test/java/davor/springframework/spring6restmvc/Controller/BeerControllerTest.java
+++ src/test/java/davor/springframework/spring6restmvc/Controller/BeerControllerTest.java
@@ -1,21 +1,53 @@
 package davor.springframework.spring6restmvc.Controller;
 
+import davor.springframework.spring6restmvc.model.Beer;
+import davor.springframework.spring6restmvc.services.BeerService;
+import davor.springframework.spring6restmvc.services.BeerServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.core.Is.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import java.awt.*;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
+@WebMvcTest(BeerController.class)
+//@SpringBootTest
 class BeerControllerTest {
 
+
     @Autowired
-    BeerController beerController;
+    MockMvc mockMvc;
+
+    @MockBean
+    BeerService beerService;
+
+    BeerServiceImpl beerServiceImpl = new BeerServiceImpl();
 
     @Test
-    void getBeerById() {
-        System.out.println(beerController.getBeerById(UUID.randomUUID()));
+    void getBeerById() throws Exception {
+
+        Beer testBeer = beerServiceImpl.listBeers().get(0);
+
+        given(beerService.getBeerById(testBeer.getId()))
+                .willReturn(testBeer);
+
+        mockMvc.perform(get("/api/v1/beer/"+testBeer.getId())
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id", is(testBeer.getId().toString())))
+                .andExpect(jsonPath("$.beerName", is(testBeer.getBeerName())));
     }
 }
